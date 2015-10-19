76b6fd42b94db42724ccb6cdbccf229ea89fbca6
icaboalo <icaboalo@gmail.com>
2015-10-19 08:35:57 -0500

    added floating button library

--- app/src/main/java/com/icaboalo/plantoeat/ui/activity/AddRecipeActivity.java
+++ app/src/main/java/com/icaboalo/plantoeat/ui/activity/AddRecipeActivity.java
@@ -3,18 +3,43 @@ package com.icaboalo.plantoeat.ui.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.NumberPicker;
+import android.widget.Toast;
 
 import com.icaboalo.plantoeat.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class AddRecipeActivity extends AppCompatActivity {
+
+    @Bind(R.id.app_bar)
+    Toolbar mToolbar;
+
+    @Bind(R.id.numberPicker)
+    NumberPicker mNumberPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mNumberPicker.setMaxValue(10);
+        mNumberPicker.setMinValue(0);
+        mNumberPicker.setWrapSelectorWheel(false);
+
+
+
+        mNumberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                int number = mNumberPicker.getValue();
+                Toast.makeText(AddRecipeActivity.this, newVal + "", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
