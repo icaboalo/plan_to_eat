9717e96856466409d1063630aad97fe87927337f
icaboalo <icaboalo@gmail.com>
2015-10-21 17:23:28 -0500

    finished ingredients recycler

---
+++ app/src/main/java/com/icaboalo/plantoeat/domain/Ingredients.java
@@ -0,0 +1,26 @@
+package com.icaboalo.plantoeat.domain;
+
+/**
+ * Created by icaboalo on 10/21/2015.
+ */
+public class Ingredients {
+
+    String mIngredientName;
+    String mQuantity;
+
+    public String getIngredientName() {
+        return mIngredientName;
+    }
+
+    public String getQuantity() {
+        return mQuantity;
+    }
+
+    public void setIngredientName(String ingredientName) {
+        mIngredientName = ingredientName;
+    }
+
+    public void setQuantity(String quantity) {
+        mQuantity = quantity;
+    }
+}
